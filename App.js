c331d7503e135afe5ee55d873d3d6c16bd952968
Dvijesh Patel <dpatel@tekion.com>
2022-09-23 20:07:08 +0530

    video 3

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  return <View style={styles.container}>
+    <View style={styles.header}>
+      <Text style={styles.boldText}>
+        Hello, World!
+      </Text>
     </View>
-  );
+    <View style={styles.body}>
+      <Text style={styles.boldText}>Lorem ipsom <Text>test</Text> doloar sit amet</Text>
+      <Text>Lorem ipsom doloar sit amet</Text>
+      <Text>Lorem ipsom doloar sit amet</Text>
+    </View>
+    </View>
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +24,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText:  {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+    fontWeight: 'bold',
+  }
 });
